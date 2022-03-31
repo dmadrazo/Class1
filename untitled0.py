0b4b79b7212b275ffc1d8caf5527948abff6f70f
Diego Madrazo <diegomadrazo@uchicago.edu>
2022-03-31 11:15:44 -0500

    Create untitled0.py
    Create program

---
+++ untitled0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 11:09:08 2022
+
+@author: diego
+"""
+
+
+a = [1,2,3,4,5]
+b = []
+
+for i in a:
+      b.append(i*2)
+      
+print(b)
